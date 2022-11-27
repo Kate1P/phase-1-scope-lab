0a64e4663d89b08634b143a1297b8ddd42d195ac
Kate Popova <popova1ea@gmail.com>
2022-11-26 16:39:29 -0800

    commit tree ea47984c4eaa57956a9544d2a4054715f4f70a8d

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = 'Kate'
+function upperCaseCustomerName() {
+   return customerName = customerName.toUpperCase(); 
+}
+function setBestCustomer() {
+   return bestCustomer = "not bob"; }
+function overwriteBestCustomer() {
+   return bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer() {
+   return leastFavoriteCustomer = 'bob'; 
+}
